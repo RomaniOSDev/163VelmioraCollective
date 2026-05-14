5ead1eba0d8d4cebd87b56a37c83db7fbe82798c
Roman <romazaniosdev@gmail.com>
2026-05-14 23:13:12 +0300

    Initial Commit

---
+++ 163VelmioraCollective/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  163VelmioraCollective
+//
+//  Created by Roman on 5/14/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
